446c3a0bdf3eb21acc60c55f816f0ae3a5e9f242
Daniel Pires <danieldppires@gmail.com>
2025-02-20 11:31:05 -0300

    Featured Posts

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -23,8 +23,87 @@ const FeaturedPosts = () => {
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
 				</Link>
 			</div>
+
 			{/* Others */}
-			<div className="w-full lg:w-1/2 flex flex-col gap-4"></div>
+			<div className="w-full lg:w-1/2 flex flex-col gap-4">
+				{/* Second */}
+				<div className="lg:h-1/3 flex justify-between gap-4">
+					<Image
+						src="featured2.jpeg"
+						className="rounded-3xl object-cover w-1/3 aspect-video"
+					/>
+					{/* details and title*/}
+					<div className="w-2/3">
+						{/* details */}
+						<div className="flex items-center gap-4 text-sm mb-4">
+							<h2 className="font-semibold">02.</h2>
+							<Link to="/" className="text-cyan-600">
+								Development
+							</Link>
+							<span className="text-gray-500 text-xs">2 days ago</span>
+						</div>
+						<Link
+							to="/test"
+							className="text-base sm:text-lg md:text-xl font-medium"
+						>
+							Officiis cupiditate maxime ipsum odit! Perspiciatis hic nemo
+							inventore?
+						</Link>
+					</div>
+				</div>
+
+				{/* Third */}
+				<div className="lg:h-1/3 flex justify-between gap-4">
+					<Image
+						src="featured3.jpeg"
+						className="rounded-3xl object-cover w-1/3 aspect-video"
+					/>
+					{/* details and title*/}
+					<div className="w-2/3">
+						{/* details */}
+						<div className="flex items-center gap-4 text-sm mb-4">
+							<h2 className="font-semibold">03.</h2>
+							<Link to="/" className="text-cyan-600">
+								Web Design
+							</Link>
+							<span className="text-gray-500 text-xs">2 days ago</span>
+						</div>
+						<Link
+							to="/test"
+							className="text-base sm:text-lg md:text-xl font-medium"
+						>
+							Voluptate enim debitis porro nobis iure corrupti mollitia
+							assumenda.
+						</Link>
+					</div>
+				</div>
+
+				{/* Fourth */}
+				<div className="lg:h-1/3 flex justify-between gap-4">
+					<Image
+						src="featured4.jpeg"
+						className="rounded-3xl object-cover w-1/3 aspect-video"
+					/>
+					{/* details and title*/}
+					<div className="w-2/3">
+						{/* details */}
+						<div className="flex items-center gap-4 text-sm mb-4">
+							<h2 className="font-semibold">04.</h2>
+							<Link to="/" className="text-cyan-600">
+								Search Engines
+							</Link>
+							<span className="text-gray-500 text-xs">2 days ago</span>
+						</div>
+						<Link
+							to="/test"
+							className="text-base sm:text-lg md:text-xl font-medium"
+						>
+							Aliquid voluptate saepe quod quis neque assumenda mollitia eaque
+							harum adipisci cum. Nobis.
+						</Link>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -19,12 +19,12 @@ const Image = ({
 		<IKImage
 			urlEndpoint={import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT}
 			path={src}
-			className={className}
 			loading="lazy"
 			lqip={{ active: true, quality: 20 }}
 			alt={alt}
 			width={w}
 			height={h}
+			className={className}
 		/>
 	);
 };
